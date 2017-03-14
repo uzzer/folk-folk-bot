95ce5de575f9527f964e3b36de678bc4e7596879
Mihhail Sidorin <mihhail.sidorin@folkefinans.com>
2017-03-14 09:34:06 +0200

    Allow emoji before morning word
    TODO: Probably it is too much detailed change

--- features/morning_responder.rb
+++ features/morning_responder.rb
@@ -1,7 +1,7 @@
 class MorningResponder
   class << self
     def responds_to?(msg)
-      return true if msg =~ /^(goo+d)*.{0,1}mo+rning.*/i
+      return true if msg =~ /^(:[a-z]+:)*(\s)*(goo+d)*.{0,1}mo+rning.*/i
       return true if msg == 'Góðan daginn!'
       false
     end
